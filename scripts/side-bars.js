447aba4ccc10ab5d9d1003a88a22e8aac61a2d45
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-10-20 21:17:32 +0200

    no energy to explain

--- scripts/side-bars.js
+++ scripts/side-bars.js
@@ -4,6 +4,7 @@ const tweetBTN = document.querySelector('.sidebar-tweet');
 const feed = document.querySelector('.feed');
 const widgetsBar = document.querySelector('.widgets');
 const searchBar = document.querySelector('.widgets-input');
+const searchInput = document.querySelector('.search-input')
 
 logo.addEventListener('click', ()=>{
   navBar.classList.toggle('active');
@@ -11,5 +12,19 @@ logo.addEventListener('click', ()=>{
   tweetBTN.classList.toggle('active');
   feed.classList.toggle('active');
   widgetsBar.classList.toggle('active');
+
+});
+
+
+
+searchBar.addEventListener('mouseover', ()=>{
   searchBar.classList.toggle('active');
+  searchInput.style.transition = "1s";
+  searchInput.style.backgroundColor = "#1c39BB";
+});
+
+searchBar.addEventListener('mouseout', ()=>{
+  searchBar.classList.toggle('active');
+  searchInput.style.backgroundColor = "#0067A5";
+    searchInput.style.transition = "1s";
 });
